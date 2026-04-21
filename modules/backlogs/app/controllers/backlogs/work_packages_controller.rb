7ac63b0c251324832c1850e0550dfe94cc72208f
Ivan Kuchin <i.kuchin@openproject.com>
2026-04-21 21:35:46 +0200

    WIP fix moving

--- modules/backlogs/app/controllers/backlogs/work_packages_controller.rb
+++ modules/backlogs/app/controllers/backlogs/work_packages_controller.rb
@@ -31,6 +31,7 @@
 module Backlogs
   class WorkPackagesController < BaseController
     include OpTurbo::ComponentStream
+    include Backlogs::Move
 
     before_action :load_story
 
@@ -130,19 +131,6 @@ module Backlogs
 
       # Update the target component so that the moved story shows up
       replace_sprint_component_via_turbo_stream(sprint: new_sprint)
-    end
-
-    def infer_attributes_from_target
-      target_type, target_id = move_params[:target_id].split(":")
-
-      case target_type
-      when "sprint"
-        { sprint_id: target_id }
-      when "inbox"
-        { sprint_id: nil }
-      else
-        raise ArgumentError, "target_type must include one of: sprint, inbox."
-      end
     end
 
     def target_sprint?(move_attributes)
