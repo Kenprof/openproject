683eb400874a3315ef3e1f1bfb5904cf5fac67f1
devidris <info@dev-idris.com>
2026-04-22 21:23:04 +0100

    Initial commit

---
+++ frontend/src/app/shared/components/fields/display/field-types/work-package-display-field.module.ts
@@ -0,0 +1,93 @@
+//-- copyright
+// OpenProject is an open source project management software.
+// Copyright (C) the OpenProject GmbH
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See COPYRIGHT and LICENSE files for more details.
+//++
+
+import { DisplayField } from 'core-app/shared/components/fields/display/display-field.module';
+import { WorkPackageResource } from 'core-app/features/hal/resources/work-package-resource';
+
+export class WorkPackageDisplayField extends DisplayField {
+  public text = {
+    none: this.I18n.t('js.filter.noneElement'),
+  };
+
+  public get value() {
+    return this.resource[this.name];
+  }
+
+  public get title() {
+    if (this.isEmpty()) {
+      return this.text.none;
+    }
+    return this.value.name;
+  }
+
+  public get wpId() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    if (this.value.$loaded) {
+      return this.value.id;
+    }
+
+    // Read WP ID from href
+    return this.value.href.match(/(\d+)$/)[0];
+  }
+
+  /**
+   * Returns the work package ID formatted for display.
+   * Classic mode: `#123` (hash-prefixed), Semantic mode: `PROJ-42` (no prefix).
+   *
+   * Delegates to `WorkPackageResource#formattedId` when the linked resource
+   * is loaded. When unloaded, falls back to the numeric ID extracted from
+   * the self-link href (which has no `displayId` available).
+   */
+  public get wpFormattedId():string {
+    const linkedWp = this.value as WorkPackageResource | undefined;
+    if (linkedWp?.$loaded) {
+      return linkedWp.formattedId;
+    }
+
+    const id = this.wpId as string | number | null;
+    if (!id) return '';
+
+    return `#${id}`;
+  }
+
+  public get valueString() {
+    // cannot display the type name easily here as it may not be loaded
+    return `${this.wpFormattedId} ${this.title}`;
+  }
+
+  public isEmpty():boolean {
+    return !this.value;
+  }
+
+  public get unknownAttribute():boolean {
+    return false;
+  }
+}
